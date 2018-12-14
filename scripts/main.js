6cd8aa741ed63e50da90ea43b6dc89b18413916d
Adam Parker <adam3692@hotmail.co.uk>
2018-12-14 20:49:49 +0000

    Styled services page | added new images and information

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,26 @@ $(document).ready(function() {
   var $header = $('header');
   var $sticky = $header.before($header.clone().addClass("sticky"));
 
-  //Sticky menu
-  $(window).on("scroll", function() {
-    var scrollFromTop = $(window).scrollTop();
-    $("body").toggleClass("scroll", (scrollFromTop > 350));
-  });
-
   // Make sure scripts run on certain pages START
-  console.log(window.location.pathname)
-  if(window.location.pathname === "/index.html" || window.location.pathname === "/index.html#" || window.location.pathname === "/") {
+  if (window.location.pathname === "/index.html" || window.location.pathname === "/index.html#" || window.location.pathname === "/") {
+    //Sticky menu
+    $(window).on("scroll", function() {
+      var scrollFromTop = $(window).scrollTop();
+      $("body").toggleClass("scroll", (scrollFromTop > 350));
+    });
+
     // SLICK JS
     $('.slider-grid').slick({
       infinite: true,
       autoplay: true,
-      arrows:   true,
-      dots:     true
+      arrows: true,
+      dots: true
     });
     $('.work-grid').slick({
-      infinite:     true,
-      autoplay:     true,
-      arrows:       true,
-      dots:         true,
+      infinite: true,
+      autoplay: true,
+      arrows: true,
+      dots: true,
       slidesToShow: 2
     });
 
@@ -35,22 +34,21 @@ $(document).ready(function() {
 
   //workhours menu
   $('.opening-menu').hide();
-  $('.opening').click(function(){
-    if($menu) {
-      $('.opening-menu').slideToggle("fast", function(){});
+  $('.opening').click(function() {
+    if ($menu) {
+      $('.opening-menu').slideToggle("fast", function() {});
       $('.opening').css({
         'bottom': '0',
-        'width:':  '125px'
+        'width:': '125px'
       });
       $menu = false;
     } else {
-      $('.opening-menu').slideToggle("fast", function(){});
+      $('.opening-menu').slideToggle("fast", function() {});
       $('.opening').css({
         'bottom': '370px',
-        'width':  '300px'
+        'width': '300px'
       });
       $menu = true;
     }
   });
-
 })
