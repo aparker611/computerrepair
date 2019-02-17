5e6a485288bd2621561c125fa0fbe37ea2ea6ecc
aparker611 <adam3692@hotmail.co.uk>
2019-02-17 16:25:54 +0000

    Added PHP for including header&footer instead of rewriting across files

---
+++ templates/footer.php
@@ -0,0 +1,34 @@
+<footer id="footer">
+  <div class="footer-wrapper">
+    <p class="copywrite">Copyright © 2018 Adam Parker</p>
+    <a href="https://www.facebook.com/adamcomputertech" target="_blank" class="social-item"><img src="images/facebook.svg" alt="" class="social-img"></a>
+    <a href="https://www.linkedin.com/in/adam-parker-910029101/" target="_blank" class="social-item"><img src="images/linkedin.svg" alt="" class="social-img"></a>
+  </div>
+  <section class="opening">
+    <p class="opening-title">Opening Hours</p>
+  </section>
+  <section class="opening-menu">
+    <h2 class="sub-title menu">Available hours</h2>
+    <ul class="menu">
+      <li class="menu-item">MONDAY - 9AM-8PM</li>
+      <li class="menu-item">TUESDAY - 9AM-8PM</li>
+      <li class="menu-item">WEDNESDAY - 9AM-8PM</li>
+      <li class="menu-item">THURSDAY - 9AM-8PM</li>
+      <li class="menu-item">FRIDAY - 9AM-8PM</li>
+      <li class="menu-item">SATURDAY - 10AM-8PM</li>
+      <li class="menu-item">SUNDAY - CLOSED</li>
+    </ul>
+  </section>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
+<script src="scripts/slick.min.js"></script>
+<script src="scripts/parallax.min.js"></script>
+<script src="scripts/particles.min.js"></script>
+<script src="scripts/masonry.js"></script>
+<script src="scripts/form_validation.js"></script>
+<script src="scripts/detectmobile.min.js"></script>
+<script src="scripts/main.js"></script>
+</body>
+
+</html>
